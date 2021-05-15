8aaf0a7915bcfe88f445491ea478e07d86c7a28c
tonyaellie <tonyaellie@gmail.com>
2021-05-15 23:19:14 +0000

    initial

---
+++ src/app.js
@@ -0,0 +1,63 @@
+
+const chalk = require('chalk');
+const util = require('util');
+const EventEmitter = require('events');
+
+class loggerEvents extends EventEmitter {}
+
+const events = new loggerEvents();
+
+const parseArguments = (args) => {
+  return args.map((item) => {
+    if (typeof item === 'string') {
+      return item;
+    }
+
+    return util.inspect(item, { colors: false, depth: null });
+  });
+};
+
+const prepareMessage = (chalkStyle, prefix, args) => {
+  const msg = chalkStyle(`(${new Date().toLocaleTimeString()}) - ${chalk.bold(prefix)} - `, ...parseArguments(Array.prototype.slice.call(args)));
+  events.emit('all', msg);
+  return msg;
+};
+
+const log = (...arguments) => {
+  console.log(prepareMessage(chalk.white, '[LOG]', arguments));
+};
+
+const info = (...arguments) => {
+  console.info(prepareMessage(chalk.cyan, '[INFO]', arguments));
+};
+
+const error = (...arguments) => {
+  console.error(prepareMessage(chalk.red, '[ERROR]', arguments));
+};
+
+const fatal = (...arguments) => {
+  console.error(prepareMessage(chalk.red, '[FATAL]', arguments));
+};
+
+const warn = (...arguments) => {
+  console.warn(prepareMessage(chalk.yellow, '[WARN]', arguments));
+};
+
+const debug = (...arguments) => {
+  console.debug(prepareMessage(chalk.green, '[DEBUG]', arguments));
+};
+
+const clear = () => {
+  console.clear();
+};
+
+module.exports = {
+  events,
+  log,
+  info,
+  error,
+  fatal,
+  warn,
+  debug,
+  clear
+};
